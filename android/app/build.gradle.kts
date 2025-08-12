fe8c1bcad5e11a98e162c6c806d3e1fec9a610d9
Samet Yılmaz <yilmazsamet98@gmail.com>
2025-08-12 16:29:00 +0300

    Fix: app customization WLK31

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.wishlink"
+    namespace = "com.astavist.wishlink"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.wishlink"
+        applicationId = "com.astavist.wishlink"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
