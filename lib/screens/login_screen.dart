2d0190e2853c0c961b447d3570fbb00e3f51dc87
Samet Yılmaz <yilmazsamet98@gmail.com>
2025-12-18 17:02:53 +0300

    Bug: Login screen wrong logo WLK94

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -990,8 +990,8 @@ class _LoginScreenState extends State<LoginScreen> {
     final mediaQuery = MediaQuery.of(context);
     final showAppleSignIn = !kIsWeb && theme.platform == TargetPlatform.iOS;
     final logoAsset = theme.brightness == Brightness.dark
-        ? 'assets/images/LogoBlack.png'
-        : 'assets/images/Logo.png';
+        ? 'assets/images/Logo.png'
+        : 'assets/images/LogoBlack.png';
 
     return Scaffold(
       resizeToAvoidBottomInset: true,
